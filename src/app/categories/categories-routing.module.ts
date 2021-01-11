722dc5b9f8cbbd336af132f44e866716c33ced6d
Vondreii <Sharlene.VonDrehnen@outlook.com>
2021-01-11 16:11:32 +1100

    WIP docos on separate pages

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from '../about/about.component';
 import { DocumentariesComponent } from '../documentaries/documentaries.component';
 import { HomeComponent } from '../home/home.component';
 import { CategoriesComponent } from './categories.component';
