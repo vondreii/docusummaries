82f0bc19c1d7d32f04ef33e2c81cd24e7ec9dadd
Vondreii <Sharlene.VonDrehnen@outlook.com>
2021-01-11 16:11:00 +1100

    WIP docos on separate pages

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
 import { DocumentariesComponent } from '../documentaries/documentaries.component';
+import { HomeComponent } from '../home/home.component';
 import { CategoriesComponent } from './categories.component';
 
 const routes: Routes = [
@@ -10,7 +12,10 @@ const routes: Routes = [
   { path: 'murders', component: DocumentariesComponent },
   { path: 'physical', component: DocumentariesComponent },
   { path: 'psychology', component: DocumentariesComponent },
-  { path: 'shootings', component: DocumentariesComponent }
+  { path: 'shootings', component: DocumentariesComponent },
+
+  
+  { path: 'aircrash/example', component: HomeComponent },
 ];
 
 @NgModule({
