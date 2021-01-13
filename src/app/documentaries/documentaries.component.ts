ae96d073086d9a3d7c1aba89de206b43f4e0a4a0
Vondreii <Sharlene.VonDrehnen@outlook.com>
2021-01-13 11:21:26 +1100

    Fully functional full website searchbar - can now search by category, tag, or documentary

--- src/app/documentaries/documentaries.component.ts
+++ src/app/documentaries/documentaries.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DocumentaryService } from '../services/documentary.service';
 import { TagService } from '../services/tag.service';
 import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-documentaries',
@@ -16,24 +17,32 @@ export class DocumentariesComponent implements OnInit {
   // Tag and documentary data from DB
   tagsList: any;
   documentariesList: any;
+  categoryList: any;
 
   // Used for only displaying current categories
   currentTagId: string = "";
   currentTag: string = "";
+  currentCategoryId: string = "";
+  currentCategory: string = "";
+  isTag: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
+    private categoryService: CategoryService,
     private tagService: TagService,
     private docoService: DocumentaryService
   ) { 
     // params is the :id (unique value) from the link (eg 'aircrash' from /categories/aircrash). 
     this.route.params.subscribe(params => {
       this.listDocos();
+      console.log(params);
     });
   }
 
   // Provide a list of documentaries
   ngOnInit(): void {
+    this.currentTag = "";
+    this.currentCategory = "";
     this.listDocos();
   }
 
@@ -41,8 +50,16 @@ export class DocumentariesComponent implements OnInit {
   listDocos() {
     let url = window.location.href;
     this.linkRoute = url.substring(url.lastIndexOf("/")+1, url.length);
-    this.getCurrentTag();
     this.documentariesList = this.docoService.readFromDB();
+
+    this.currentTag = "";
+    this.currentCategory = "";
+    this.currentTagId = "";
+    this.currentCategoryId = "";
+
+
+    this.getCurrentTag();
+    this.getCurrentCategory();
   }
 
   // Find the current tag of the page (eg, find if the user selected 'aircrash'). 
@@ -54,6 +71,18 @@ export class DocumentariesComponent implements OnInit {
         this.currentTag = tag.name;
       }
     });
-    console.log("Tag selected: " + this.currentTag);
+    console.log("Tag Pipeline: " + this.currentTag);
+  }
+
+  // Find the current category of the page (eg, find if the user selected 'health'). 
+  async getCurrentCategory() {
+    this.categoryList = await this.categoryService.readFromDB();
+    this.categoryList.forEach(category => {
+      if(this.linkRoute.match(category.link.toString())){
+        this.currentCategoryId = category.id;
+        this.currentCategory = category.name;
+      }
+    });
+    console.log("Category Pipeline: " + this.currentCategory);
   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TagService } from '../services/tag.service';
 import { DocumentaryService } from '../services/documentary.service';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-search',
@@ -13,25 +14,36 @@ export class SearchComponent implements OnInit {
   searchText = '';
   documentariesList: any;
   tagsList: any;
+  categoriesList: any; 
   searchString: Array<string> = new Array<string>();
 
   constructor(
+    private categoryService: CategoryService,
     private docoService: DocumentaryService,
     private tagService: TagService
   ) { }
 
   // Build search string
   ngOnInit(): void {
+    this.addCatagories();
     this.addTags();
     this.addDocos();
     console.log("Search string created." + this.searchString);
   }
 
   // Pull categories list from Firebase and add to the search string
+  async addCatagories() {
+    this.categoriesList = await this.categoryService.readFromDB();
+    this.categoriesList.forEach(category => {
+      this.searchString.push(category.name + " (Category) -@/categories/" + category.link);
+    });
+  }
+
+  // Pull categories list from Firebase and add to the search string
   async addTags() {
     this.tagsList = await this.tagService.readFromDB();
     this.tagsList.forEach(tag => {
-      this.searchString.push(tag.name + " (Category) -@/categories/" + tag.link);
+      this.searchString.push("#" + tag.name + " -@/categories/" + tag.link);
     });
   }
 
